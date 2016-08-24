3bd98823c9fdc4d5302b317e6fa4ecf4dd568351
Tele-Fox <Tele-Fox@users.noreply.github.com>
2016-08-24 12:15:16 +0430

    Write cyr.sh in .run folder

---
+++ .run/cyr.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+cyr() {
+ chmod 777 1.sh
+ chmod 777 2.sh
+ chmod 777 3.sh
+ }
+ 
+ if [ "$1" = "r" ]; then
+  adv
+ else
+ echo "Use:"
+ echo "$0 r"
+ #This is a helper for create anti spam bot.
+ #created by: @ILoveItaly and powered by: #FoxTeam
+ #warning: CopyRight all right reserved, we can block you channel or bot.
+fi
